b6d3107d275d15897641a4baafea916948ef5262
ManInDark <61268856+ManInDark@users.noreply.github.com>
2024-10-17 15:45:32 +0200

    added subscription to update todos dynamically

--- src/app/services/todolist.service.ts
+++ src/app/services/todolist.service.ts
@@ -6,38 +6,39 @@ import { PocketBaseService } from "./pocketbase.service";
 })
 export class TodoListService {
     pocketbase = inject(PocketBaseService)
-    list: Array<string> = []
-    id: string = "";
-    getList() { return this.list; }
-    setList(list: Array<string>) { this.list = list; this.saveList(); }
+    #list: Array<string> = []
+    #id: string = "";
+    get id() { return this.#id; }
+    set id(id: string) { this.#id = id; }
+    get list() { return this.#list; }
+    set list(list: Array<string>) { this.#list = list; }
     addEntry(entry: string) { this.list.push(entry); this.saveList(); }
     removeEntry(entry: string) { this.list.splice(this.list.indexOf(entry), 1); this.saveList(); }
     saveList() {
-        localStorage.setItem("todo", JSON.stringify(this.list));
-        if (this.pocketbase.pb.authStore.isValid) {
-            const data = { "user": this.pocketbase.pb.authStore.model.id, "entries": this.list };
-            if (this.id !== "") {
-                this.pocketbase.pb.collection("todo").update(this.id, data);
-            }
+        if (this.pocketbase.pb.authStore.isValid && this.id !== "") {
+            this.pocketbase.pb.collection("todo").update(this.id, { 
+                "user": this.pocketbase.pb.authStore.model.id,
+                 "entries": this.list
+            });
         }
     }
     loadList() {
-        let storedData = localStorage.getItem("todo");
-        this.setList(storedData !== null ? JSON.parse(storedData) : []);
         if (this.pocketbase.pb.authStore.isValid) {
             this.pocketbase.pb.collection("todo").getFullList().then((data: [{ "entries": [], "id": string }]) => {
                 if (data.length > 0) {
-                    this.setId(data[0].id);
-                    this.setList(data[0].entries);
+                    this.id = data[0].id;
+                    this.list = data[0].entries;
+                    this.pocketbase.pb.collection('todo').subscribe('*', (data: { record: { id: string, entries: Array<string> } }) => {
+                        this.list = data.record.entries;
+                    }, {});
                 } else {
                     const data = { "user": this.pocketbase.pb.authStore.model.id, "entries": this.list };
-                    console.log(this.pocketbase.pb.collection("todo").create(data));
+                    this.pocketbase.pb.collection("todo").create(data);
+                    this.loadList();
                 }
             });
         }
     }
-    getId() { return this.id; }
-    setId(id: string) { this.id = id; }
     constructor() {
         this.loadList();
     }
